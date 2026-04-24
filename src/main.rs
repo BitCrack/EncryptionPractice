bbfa495e978fdee49ed48a2d40c81d49243994ef
BitCrack <matik04@gmail.com>
2026-04-24 17:31:18 +0200

    Got the example QT project running

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+pub mod cxxqt_object;
+
+use cxx_qt::casting::Upcast;
+use cxx_qt_lib::{QGuiApplication, QQmlApplicationEngine, QQmlEngine, QUrl};
+use std::pin::Pin;
+
 fn main() {
-    println!("Hello, world!");
+    // Create the application and engine
+    let mut app = QGuiApplication::new();
+    let mut engine = QQmlApplicationEngine::new();
+
+    // Load the QML path into the engine
+    if let Some(engine) = engine.as_mut() {
+        engine.load(&QUrl::from("qml/main.qml"));
+    }
+
+    if let Some(engine) = engine.as_mut() {
+        let engine: Pin<&mut QQmlEngine> = engine.upcast_pin();
+        // Listen to a signal from the QML Engine
+        engine
+            .on_quit(|_| {
+                println!("QML Quit!");
+            })
+            .release();
+    }
+
+    // Start the app
+    if let Some(app) = app.as_mut() {
+        app.exec();
+    }
 }
